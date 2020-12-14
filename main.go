5ca2a1a9d43605845a266a4e4c875922d209fac9
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-12-14 00:03:13 +0000

    Reading through the vm directory and listing available vms
    Signed-off-by: Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>

--- main.go
+++ main.go
@@ -4,19 +4,43 @@ import (
 	"os"
 	"fmt"
 	"os/exec"
-	// "log"
+	"log"
+	"io/ioutil"
+	"strconv"
 )
 
 func main() {
 	// Checking if VMware is installed
 	if _, err := os.Stat("/Applications/VMware Fusion.app"); os.IsNotExist(err) {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 
-	// Checking if vmrun is in the PATH
+	// Checking if vmrun is available
 	cmd := exec.Command("vmrun")
 	err := cmd.Start()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(homeDir + "/Virtual Machines.localized")
+
+	if _, err := os.Stat(homeDir + "/Virtual Machines.localized"); os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	files, err := ioutil.ReadDir(homeDir + "/Virtual Machines.localized")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, f := range files {
+		if f.Name() != ".DS_Store" && f.Name() != ".localized" {
+			fmt.Println(strconv.Itoa(i) + " " + f.Name())
+		}
 	}
 }

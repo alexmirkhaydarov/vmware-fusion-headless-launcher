8067b5dcb1278bf0d2f7d69fe037aad35ce27062
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-12-19 23:52:01 +0000

    [WIP] Initial stage - able to run vmrun and start a vm
    constructing a scruct inside a for a loop looks bad
    
    Signed-off-by: Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"path/filepath"
-	"os"
 	"fmt"
-	"os/exec"
-	"log"
 	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strconv"
 )
 
@@ -32,14 +32,58 @@ func main() {
 		log.Fatal(err)
 	}
 
-	files, err := ioutil.ReadDir(homeDir + "/Virtual Machines.localized")
+	dir, err := ioutil.ReadDir(homeDir + "/Virtual Machines.localized")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for i, f := range files {
+	fmt.Println("> Select a value")
+
+	for i, f := range dir {
 		if filepath.Ext(f.Name()) == ".vmwarevm" {
-			fmt.Println(strconv.Itoa(i - 1) + " " + f.Name())
+			fmt.Println(strconv.Itoa(i-1) + " " + f.Name())
+		}
+	}
+
+	fmt.Println("------------------------------------------")
+	var input int
+	fmt.Scan(&input)
+	// fmt.Println(input)
+
+	for i, f := range dir {
+		if i - 1 == input {
+			// fmt.Println(f.Name())
+			// fmt.Println(homeDir + "/Virtual Machines.localized" + "/" + f.Name())
+			dir2, err := ioutil.ReadDir(homeDir + "/Virtual Machines.localized" + "/" + f.Name())
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, d := range dir2 {
+				if filepath.Ext(d.Name())  == ".vmx" {
+					// fmt.Println(homeDir + "/Virtual Machines.localized" + "/" + f.Name() + "/" + d.Name())
+
+					fullPath := homeDir + "/Virtual Machines.localized" + "/" + f.Name()
+					vmxImage := d.Name()
+
+					vmrunPath, _ := exec.LookPath("vmrun")
+					cmdRun := &exec.Cmd {
+						Path: vmrunPath,
+						Args: []string{ vmrunPath, "-T", "fusion", "start", vmxImage, "nogui"},
+						Dir: fullPath,
+						Stdout: os.Stdout,
+						Stderr: os.Stderr,
+					}
+
+					// fmt.Println( cmdRun.String() )
+
+					if err := cmdRun.Run(); err != nil {
+						fmt.Println("Error:", err)
+					}
+				}
+				// 	fmt.Println(d.Name())
+			}
+			// fmt.Println(dir2)
 		}
 	}
 }
